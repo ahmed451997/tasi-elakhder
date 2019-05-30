a847ebc6214b43b6c6352b60dcefd4f5f8750f94
ahmed451997 <50491943+ahmed451997@users.noreply.github.com>
2019-05-30 13:46:09 +0200

    Add files via upload

---
+++ AutoDM.js
@@ -0,0 +1,48 @@
+const T = require("./Twit.js");
+const my_user_name = require("../config").userName;
+const timeout = 1000 * 60 * 5; // timeout to send the message 5 min
+
+const AutoDM = () => {
+  const stream = T.stream("user");
+  console.log("Start Sending Auto Direct Message 🚀🚀🚀");
+  stream.on("follow", SendMessage);
+};
+
+const SendMessage = user => {
+  const { screen_name, name } = user.source;
+
+  const obj = {
+    screen_name,
+    text: GenerateMessage(name)
+  };
+  // the follow stream track if I follow author person too.
+  if (screen_name != my_user_name) {
+    console.log(" 🎉🎉🎉🎉 New Follower  🎉🎉🎉🎉🎉 ");
+    setTimeout(() => {
+      T.post("direct_messages/new", obj)
+        .catch(err => {
+          console.error("error", err.stack);
+        })
+        .then(result => {
+          console.log(`Message sent successfully To  ${screen_name}  💪💪`);
+        });
+    }, timeout);
+  }
+};
+const GenerateMessage = name => {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  const d = new Date();
+  const dayName = days[d.getDay()];
+  return `السلام عليكم يا ${name} شكرا لمتابعتي .... \n يوم سعيد ${dayName} 😊😊 `; // your message
+  // My message   return `السلام عليكم ${name} شكرا لمتابعتي معاك احمد اشرف محلل فني و موصي جيد لسوق الاسهم السعودي..هل تتداول في سوق الاسهم السعودي ؟.\n Happy to discuss anytime 😊  \n Happy ${dayName} 😊😊 `;
+};
+
+module.exports = AutoDM;
